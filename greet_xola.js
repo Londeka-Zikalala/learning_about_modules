781d818b9c5eb514bcda2d233169ee2d7e33f5c6
Londeka Zikalala <londekazikalala@gmail.com>
2023-07-20 14:55:42 +0200

    using external modules

--- greet_xola.js
+++ greet_xola.js
@@ -1,4 +1,10 @@
+import chalk from 'chalk';
+//import the greet module that is in the current folder
 import greet from './greet.js'
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
 
 
   
