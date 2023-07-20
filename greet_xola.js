c064d20e9acf543207044b2758eec6dd2c84d681
Londeka Zikalala <londekazikalala@gmail.com>
2023-07-20 15:07:32 +0200

    using figlet

--- greet_xola.js
+++ greet_xola.js
@@ -1,10 +1,12 @@
-import chalk from 'chalk';
-//import the greet module that is in the current folder
-import greet from './greet.js'
-
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+import figlet from "figlet";
+import greet from "./greet.js";
 
 
-
-  
+figlet(greet, function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
